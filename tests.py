3f0f960ee1d55da4a3be42c240d4e7ecc92dd6b8
lajonss <gfs.b16@gmail.com>
2017-05-16 19:43:02 +0200

    Simple test

--- tests.py
+++ tests.py
@@ -1,3 +1,29 @@
 import pyeureka
 
-print('elo')
+app = 'pyeureka-test'
+eureka_url = 'http://localhost:8765'
+heartbeat = 5.0
+instance = {
+    'ipAddr': '127.0.0.1',
+    'app': app
+}
+
+print("Eureka address: {}".format(eureka_url))
+print("Service definition")
+print(instance)
+service_wrapper = pyeureka.SimpleEurekaServiceWrapper(
+    eureka_url, instance, heartbeat)
+
+print("Registering service")
+service_wrapper.run()
+
+print("Creating client")
+client_wrapper = pyeureka.SimpleEurekaClientWrapper(eureka_url)
+
+print("Fetching app data")
+print(client_wrapper.app(app))
+
+print("Stopping service")
+service_wrapper.stop()
+
+print("Done.")
