9a0ef967969c3f1f1a2929129fec3c8d5a7cc8d3
lajonss <gfs.b16@gmail.com>
2018-03-14 00:40:21 +0100

    pypi setup script

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+setup(
+    name="pyeureka",
+    version="2018.03.14.1",
+    author="Paweł Białas",
+    author_email="gfs.b16@gmail.com",
+    url="https://github.com/lajonss/pyeureka",
+    description="Netflix Eureka REST API access library",
+    packages=["pyeureka"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries"
+    ]
+)
