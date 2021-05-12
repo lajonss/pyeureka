f5f89fbe5518fe9c1890efeb9d4095b71b07bdfc
lajonss <l3n1@dupaw.eu>
2021-05-12 14:31:03 +0200

    setup.py: requests as dependency

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from distutils.core import setup
 setup(
     name="pyeureka",
-    version="2018.03.14.1",
+    version="2021.5.12.0",
     author="Paweł Białas",
     author_email="gfs.b16@gmail.com",
     url="https://github.com/lajonss/pyeureka",
     description="Netflix Eureka REST API access library",
     packages=["pyeureka"],
+    install_requires=['requests'],
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
